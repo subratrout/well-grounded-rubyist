aefeb9f177c6296cecc5a2978cfc86f7bd0d2ce4
subratrout <subratnrout@gmail.com>
2016-04-11 22:22:55 -0600

    Added ch-2 object methods

--- ch-02/object.rb
+++ ch-02/object.rb
@@ -25,3 +25,43 @@ puts "The id of string_2 is #{string_2.object_id}."
 
 
 # Querying an object’s abilities with the respond_to? method
+obj4 = Object.new
+
+if obj4.respond_to?("talk")
+  obj.talk
+else
+  puts "Sorry, the object doesn't understand the 'talk' message."
+end
+
+# Using __send__ or public_send instead of send
+
+
+
+# METHOD ARGUMENTS
+# Method with any number of arguments
+obj5 = Object.new
+
+def obj5.multi_args(*x)
+  puts "I can take zero or more arguments!"
+end
+
+
+# Fine tune number of arguments 
+
+def obj5.two_or_more(a, b, *c)
+  puts "This method requires two or more arguments."
+  puts "And sure, I got: "
+  p a, b , c
+end
+
+
+obj5.two_or_more(1,3,5,7,9)
+
+
+# Default value for arguments
+
+def obj5.default_args(a, b, c=1)
+  puts "Values of variables in a default arg: ",a,b,c
+end
+
+obj5.default_args(3,4)
