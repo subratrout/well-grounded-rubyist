7926917e8509d2e47e674386baf3e0f0b510070a
subratrout <subratnrout@gmail.com>
2016-05-05 16:47:09 -0600

    Ch-05 method access rules

---
+++ ch-05/baker.rb
@@ -0,0 +1,42 @@
+class Cake
+	def initialize(batter)
+		@batter = batter
+		@baked = true
+	end
+end
+
+class Egg
+end
+
+class Flour
+end
+
+class Baker
+	def bake_cake
+		@batter = []
+		pour_flour
+
+		add_egg
+		stir_batter
+		return Cake.new(@batter)
+	end
+
+	def pour_flour
+		@batter.push(Flour.new)
+	end
+
+	def add_egg
+		@batter.push(Egg.new)
+	end
+
+	def stir_batter
+	end
+
+	private :pour_flour, :add_egg , :stir_batter
+
+end
+
+# If you don’t use an explicit receiver for a method call, Ruby assumes that you want to send the message to the current object, self. Thinking logically, you can conclude that the message add_egg has an object to go to only if self is an object that responds to add_egg. In other words,
+# n other words, you can only call the add_egg instance method of Baker when self is an instance of Baker.
+
+# And when is self an instance of Baker? When any instance method of Baker is being executed. Inside the definition of bake_cake, for example, you can call add_egg, and Ruby will know what to do. Whenever Ruby hits that call to add_egg inside that method definition, it sends the message add_egg to self, and self is a Baker object.
--- ch-05/class-variable-syntax-scope-visibility.rb
+++ ch-05/class-variable-syntax-scope-visibility.rb
@@ -55,3 +55,114 @@ puts "There are #{h2.make_mates}."
 puts "Counting total cars: "
 
 puts "There are #{Car.total_count}"
+
+
+
+# When you assign to @@value in Child, you’re setting the one and only @@value variable that’s 
+# shared throughout the hierarchy—that is, by Parent and Child and any other descendant classes 
+# of either of them. The term class variable becomes a bit difficult to reconcile with the fact 
+# that two (and potentially a lot more) classes share exactly the same ones.
+
+class Parent
+	@@value = 100
+end
+
+class Child < Parent
+	@@value = 200
+end
+
+class Parent
+	puts @@value # will output 200
+end
+
+class Grantchild < Child
+	@@value = 300
+end
+
+class Parent
+	puts @@value # will output 300
+end
+
+# Class variables come in handy because they break down the dam between a class object and instances of that 
+# class. But by so doing, and especially because of their hierarchy-based scope, they take on a kind of 
+# quasi-global quality: a class variable isn’t global, but it sure is visible to a lot of objects,
+# once you add up all the subclasses and all the instances of those subclasses. But they’re also leaky. 
+# Too many objects can get hold of them.
+
+# Let’s say we wanted to create a subclass of Car called Hybrid to keep a count of manufactured (partly) 
+# electric vehicles. We couldn’t do this:
+
+class Hybrid < Car
+end
+
+hy = Hybrid.new("Honda")
+
+# because Hybrid.total_count is the same method as Car.total_count, and it wraps the same variable. Class variables 
+# aren’t reissued freshly for every subclass, the way instance variables are for every object.
+
+# we can use @@total_hybrid_count class variable but it is not the clean way to work
+
+# So the alternative is 
+
+# Maintaining per-class state with isntance variables of class objects
+
+
+# we need to maintain state on a per-class basis; and because classes are objects, that means on a per-object 
+# basis (for a certain group of objects, namely, class objects). And per-object state, whether the object
+# in question is a class or something else, suggests instance variables.
+
+# The following listing shows a rewrite of the Car class above. Two of the class variables are still there, 
+# but @@total_count has been transformed into an instance variable.
+
+class Car2
+	@@makes = []
+	@@cars = {}
+	attr_reader :make 
+
+	def initialize(make)
+		if @@makes.include?(make)
+			puts "Creating a new #{make} for car Class2"
+			@make = make
+			@@cars[make] += 1
+			self.class.total_count += 1
+		else
+			raise "No such make: #{make}"
+		end
+	end
+
+	def self.total_count
+		@total_count ||= 0
+	end
+
+	def self.total_count=(n)
+		@total_count = n
+	end
+
+	def self.add_make(make)
+		@@makes << make unless @@makes.include?(make)
+		@@cars[make] = 0
+	end
+
+	def make_mates
+		@@cars[self.make]
+	end
+
+end
+
+class Hybrid2 < Car2
+end
+
+
+Car2.add_make("Honda")
+Car2.add_make("Toyota")
+Car2.add_make("Ford")
+
+h3 = Car2.new("Honda")
+t3 = Car2.new("Toyota")
+h4 = Car2.new("Honda")
+
+
+h5 = Hybrid2.new("Honda")
+h6 = Hybrid2.new("Ford")
+
+puts "There are #{Hybrid2.total_count} hybrid on the road."
