c2139ee9a0da3be56df42244d99296235e19425c
subratrout <subratnrout@gmail.com>
2016-05-04 18:42:41 -0600

    ch-05 class variable syntax and scope visibility

--- ch-05/class-variable-syntax-scope-visibility.rb
+++ ch-05/class-variable-syntax-scope-visibility.rb
@@ -15,12 +15,43 @@ class Car
 
 	attr_reader :make
 
+	def initialize(make)
+		if @@makes.include?(make)
+			puts "Creating a new #{make}"
+			@make = make
+			@@cars[make] += 1
+			@@total_count += 1
+		else
+			raise "No such make: #{make}"
+		end
+	end
+
 	def self.total_count
 		@@total_count
 	end
 
 	def self.add_make(make)
 		@@makes << make unless @@makes.include?(make)
+		@@cars[make] = 0
+	end
+
+	def make_mates
+		@@cars[self.make]
 	end
 
 end
+
+Car.add_make("Honda")
+Car.add_make("Toyota")
+
+h = Car.new("Honda")
+t = Car.new("Toyota")
+h2 = Car.new("Honda")
+
+puts "Counting cars of make same make as h2..."
+
+puts "There are #{h2.make_mates}."
+
+puts "Counting total cars: "
+
+puts "There are #{Car.total_count}"
