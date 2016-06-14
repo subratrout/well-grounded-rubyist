339c0b20c37a34dbc533934da5c400b83096bc52
subratrout <subratnrout@gmail.com>
2016-06-14 17:51:48 -0600

    ch-09 manipulating arrays

--- ch-09/manipulating-arrays.rb
+++ ch-09/manipulating-arrays.rb
@@ -1,7 +1,7 @@
 # insert an element with the []= method—using the syntactic sugar that allows you to avoid the usual method-calling dot—do this:
 
-a = []
-a[0] = "first"
+a2 = []
+a2[0] = "first"
 
 #The second line is syntactic sugar for a.[]=(0,"first"). The result is a one-element array whose first (and only) element is the string "first".
 
@@ -24,3 +24,51 @@ array = ["The", "dog", "ate", "my", "homework"]
 selected_array = array.values_at(0, 3)
 
 puts selected_array
+
+# Slice method take 2 arguments (a starting index, length (canbe optional))
+
+
+# Unshift method add an object to the beginning of the array
+
+a1 = [1,2,3,4]
+a1.unshift(0)
+
+puts a1
+
+# Push or << method add an object to the end of the array
+
+a1.push(5)
+
+puts a1
+
+# The methods << and push differ in that push can take more than one argument
+a1.push(6,7,8)
+
+# Opposite to unshift and push are shift and pop.
+# Shift removes one object from the begining of the array whereas pop removes and object from end of the array
+
+# Shift and pop method return the removed object from the array
+a = [1,2,3,4,5]
+print "The original array: "
+p a
+popped = a.pop
+print "The popped item: "
+puts popped
+
+print "The new state of the array: "
+
+p a
+
+shifted = a.shift
+
+print "The shifted item: "
+
+puts shifted
+
+print "The new state of the array: "
+
+p a
+
+# shift and pop can remove more than one element at a time. Just provide an integer argument, and that number 
+# of elements will be removed.The removed items will be returned as an array
+
